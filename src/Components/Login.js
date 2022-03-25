14eade085dd23399e78bb449f0cef008521cbb9c
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-03-26 00:52:01 +0530

    completed login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,15 +2,34 @@ import React from 'react'
 import '../Styles/Login.css';
 
 
+
 export default function Login() {
+
+
     return (
         <>
-            <div id="Login" className="card w-75 shadow p-3 mb-5 bg-body rounded-4 mx-auto my-5 " style={{ "width": "18rem" }}>
-                <div className='innerContainer d-flex'>
-                    <div id="loginImageHolder">
-                        <img src="Images/tech.webp" width="100%"  alt="" />
-                    </div>
-                    <div id="loginInputSection">right</div>
+            <div id="Login" className="w-50 shadow p-3 mb-5 bg-body mx-auto my-5 " style={{ height: "30rem", display: "flex" }}>
+                <img id="loginImage" src="Images/login.jpg" width="50%" alt="" />
+                <div className='container mx-3 my-3'>
+                    <div className='fs-5 text-center'>Login</div>
+                    <hr />
+                    <form action="">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Username</label>
+                            <input type="email" class="form-control" placeholder="username" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email</label>
+                            <input type="email" class="form-control" placeholder="abc@gmail.com" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Password</label>
+                            <input type="password" class="form-control" placeholder="password" />
+                        </div>
+                        <div className='text-center'>
+                            <button id="loginBtn" type="submit" class="btn btn-primary" style={{ "alignSelf": "center" }}>Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
